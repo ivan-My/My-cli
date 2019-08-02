4dc3be2b2bf7cd53eacfaa74d22e0e61994633dc
MingYang <mingyang@limingyangdeMacBook-Pro.local>
2019-08-02 09:02:54 +0800

    添加了可配置打包页面，优化了打包代码

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -6,7 +6,7 @@ const srcRoot = path.resolve(__dirname, 'src');
 const containerDir = path.resolve(srcRoot, 'containers');
 const mainFile = 'main.js';
 
-
+const fielConfig = ['index', 'detail',]
 function getHtmlArray(entryMap) {
     let htmlArray = [];
     Object.keys(entryMap).forEach((key) => {
@@ -26,11 +26,13 @@ function getHtmlArray(entryMap) {
 function getEntry() {
     let entryMap = {};
     fs.readdirSync(containerDir).forEach((pathname) => {
-        let fullPathName = path.resolve(containerDir, pathname);
-        let stat = fs.statSync(fullPathName);
-        let fileName = path.resolve(fullPathName, mainFile);
-        if (stat.isDirectory() && fs.existsSync(fileName)) {
-            entryMap[pathname] = fileName;
+        if (fielConfig.indexOf(pathname) != '-1' || fielConfig.length == 0) {
+            let fullPathName = path.resolve(containerDir, pathname);
+            let stat = fs.statSync(fullPathName);
+            let fileName = path.resolve(fullPathName, mainFile);
+            if (stat.isDirectory() && fs.existsSync(fileName)) {
+                entryMap[pathname] = fileName;
+            }
         }
     });
     return entryMap;
@@ -47,7 +49,7 @@ const baseConfig = {
     },
     module: {
         rules: [
-            { test: /\.(js|jsx)$/, use: [{ loader: 'babel-loader' },{loader: 'eslint-loader'}], include: srcRoot },
+            { test: /\.(js|jsx)$/, use: [{ loader: 'babel-loader' }, { loader: 'eslint-loader' }], include: srcRoot },
             {
                 test: /\.(scss|css)$/,
                 use: [MiniCssExtractPlugin.loader, {
@@ -74,7 +76,29 @@ const baseConfig = {
                     name: 'common'
                 }
             }
-        }
+        },
+        minimizer: [
+            new TerserPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true, // Must be set to true if using source-maps in production
+                terserOptions: {
+                    // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+                    compress: {
+                        // 删除所有的console语句    
+                        drop_console: true,
+                        // 把使用多次的静态值自动定义为变量
+                        reduce_vars: true,
+                    },
+                    output: {
+                        // 不保留注释
+                        // comment: false,
+                        // 使输出的代码尽可能紧凑
+                        beautify: false
+                    }
+                },
+            }),
+        ],
     },
 };
 
